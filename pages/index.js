f01d281b2eb3763ccdb55c65888dfeb7cb491a88
risarisato <nqg44226@gmail.com>
2022-08-27 08:10:55 +0900

    package-lock.jsonを追加

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          20220827ようこそ <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
